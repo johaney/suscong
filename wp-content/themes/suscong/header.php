627d7069e81dadc7bcc126febedf233b10eeec93
Johaney <johaney@joha>
2015-12-18 09:28:38 -0430

    acomodando imagen header

--- wp-content/themes/suscong/header.php
+++ wp-content/themes/suscong/header.php
@@ -46,89 +46,106 @@
 	<body>
 		<header>
 			<!--home start-->
-			<div id="home">
-				<div class="headerLine">
-					<div id="menuF" class="default">
-						<div class="container">
-							<div class="row">
-    							<div class="logo col-md-4">
-    								<div>
-	    								<a href="#">
-	    									<img src="">
-										</a>
-									</div>
-								</div>
-
-								<div class="col-md-8">
-									<div class="navmenu"style="text-align: center;">
-										<ul id="menu">
-											<li class="active" >
-												<a href="#home">Home</a>
-											</li>
-											<li>
-												<a href="#about">About</a>
-											</li>
-											<li>
-												<a href="#project">Products</a>
-											</li>
-											<li>
-												<a href="#news">Manufacturing Technologies</a>
-											</li>
-											<li class="last">
-												<a href="#contact">Contact</a>
-											</li>
-											<!--li><a href="#features">Features</a></li-->
-										</ul>
-									</div>
-								</div>
-				
-							</div>
-						</div>
+    <div id="home">
+    	<div class="headerLine">
+	<div id="menuF" class="default">
+		<div class="container">
+			<div class="row">
+				<div class="logo col-md-4">
+					<div>
+						<a href="#">
+							<img src="">
+						</a>
 					</div>
 				</div>
-		
-			<div class="container">
-				<div class="row wrap">
-					<div class="col-md-12 gallery"> 
-							<div class="camera_wrap camera_white_skin" id="camera_wrap_1">
-								<div data-thumb="" data-src="<?php bloginfo('template_url'); ?>/imagenes/slides/blank.gif">
-									<div class="img-responsive camera_caption fadeFromBottom">
-										<h2></h2>
-									</div>
-								</div>
-								<div data-thumb="" data-src="<?php bloginfo('template_url'); ?>/imagenes/slides/blank.gif">
-									<div class="img-responsive camera_caption fadeFromBottom">
-										<h2></h2>
-									</div>
-								</div>
-								<div data-thumb="" data-src="<?php bloginfo('template_url'); ?>/imagenes/slides/blank.gif">
-									<div class="img-responsive camera_caption fadeFromBottom">
-										<h2></h2>
-									</div>
-								</div>
-							</div><!-- #camera_wrap_1 -->
-
-							<div class="camera_wrap camera_white_skin" id="camera_wrap_1">
-								<div data-thumb="" data-src="<?php bloginfo('template_url'); ?>/imagenes/slides/blank.gif">
-									<div class="img-responsive camera_caption fadeFromBottom">
-										<h2></h2>
-									</div>
-								</div>
-
-								<div data-thumb="" data-src="<?php bloginfo('template_url'); ?>/imagenes/slides/blank.gif">
-									<div class="img-responsive camera_caption fadeFromBottom">
-										<h2></h2>
-									</div>
-								</div>
-							
-								<div data-thumb="" data-src="<?php bloginfo('template_url'); ?>/imagenes/slides/blank.gif">
-									<div class="img-responsive camera_caption fadeFromBottom">
-										<h2></h2>
-									</div>
-								</div>
-							</div><!-- #camera_wrap_2 -->
-						</div>
+				<div class="col-md-8">
+					<div class="navmenu"style="text-align: center;">
+						<ul id="menu">
+							<li class="active" ><a href="#home">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li><a href="#project">Products</a></li>
+							<li><a href="#news">Manufacturing Technologies</a></li>
+							<li class="last"><a href="#contact">Contact</a></li>
+							<!--li><a href="#features">Features</a></li-->
+						</ul>
 					</div>
 				</div>
 			</div>
+		</div>
+	</div>
+		<div class="container">
+			<div class="row wrap">
+				<div class="col-md-12 gallery"> 
+						<div class="camera_wrap camera_white_skin" id="camera_wrap_1">
+							<div data-thumb="" data-src="images/slides/blank.gif">
+								<div class="img-responsive camera_caption fadeFromBottom">
+									<h2></h2>
+								</div>
+							</div>
+							<div data-thumb="" data-src="images/slides/blank.gif">
+								<div class="img-responsive camera_caption fadeFromBottom">
+									<h2></h2>
+								</div>
+							</div>
+							<div data-thumb="" data-src="images/slides/blank.gif">
+								<div class="img-responsive camera_caption fadeFromBottom">
+									<h2></h2>
+								</div>
+							</div>
+						</div><!-- #camera_wrap_1 -->
+						<div class="camera_wrap camera_white_skin" id="camera_wrap_1">
+							<div data-thumb="" data-src="images/slides/blank.gif">
+								<div class="img-responsive camera_caption fadeFromBottom">
+									<h2></h2>
+								</div>
+							</div>
+							<div data-thumb="" data-src="images/slides/blank.gif">
+								<div class="img-responsive camera_caption fadeFromBottom">
+									<h2></h2>
+								</div>
+							</div>
+							<div data-thumb="" data-src="images/slides/blank.gif">
+								<div class="img-responsive camera_caption fadeFromBottom">
+									<h2></h2>
+								</div>
+							</div>
+						</div><!-- #camera_wrap_2 -->
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 project">
+					<h4>Company Profile</h4>
+					<p>Dongguan Suscong Healthcare Co., Ltd is an internationally inspired company that creates and markets Lifestyle Products, including Foot Care Accessories, Body Care and Medical Products, along with alternative accessories used across all areas of everyday living. <a href=""> Leer m&aacute;s.</a></a></p>
+				</div>
+				<div class="col-md-4 project">
+					<h4>Team</h4>
+					<p>Suscong works very diligently to provide a complete professional product and service to all our clients. In order for us to fulfill this goal we realize that teamwork and communication are the most important components. Our staff is like a family who all work together for the common good. <a href=""> Leer m&aacute;s.</a></p>
+				</div>
+				<div class="col-md-4 project">
+					<h4 style="margin-left: 20px;">Advantage</h4>
+					<p>At suscong we recognize how difficult it is for foreign companies to buy and receive a good standard of customer service in China. The difficulties are many fold; involving everything from lack of contact and information to language, delivery times and quality issues. Buying products from many different manufacturing plants can be a nightmare waiting to happen. <a href=""> Leer m&aacute;s.</a></p>
+				</div>
+			</div>
+		</div>
+	<div class="container">
+			<div class="row">
+				<div class="col-md-4 project">
+					<h4>Customer Satisfaction</h4>
+					<p>The satisfaction of our customers is our primary objective.<br> Suscong strives to attain these objectives by supplying the best quality and service. Fulfilling expectations via continuous company development with R&D and customer service instrumental in our progressive future path.</p>
+				</div>
+				<div class="col-md-4 project">
+					<h4>What do we do?</h4>
+					<p>* Sourcing.<br>* Manufacturing / Production.<br> * Packaging Solutions.<br> * Project Solutions.<br> * Research & Development.<br> * Product Enhancement.<br>* Brand Management.<br>* Quality Control.<br>* International Testing Requirement.<br>* Delivery Options.</p>
+				</div>
+				<div class="col-md-4 project">
+					<h4 style="margin-left: 20px;">Suscong is on Alibaba</h4>
+					<p><a href="www.alibaba.com">www.alibaba.com</a> a makes it easy for millions of buyers and suppliers around the world to do business online through a global trade platform for importers and exporters. Please go to our products section to direct access.</p>
+				</div>
+			</div>
+		</div>
+   
+    </div>
 		</header>
--- wp-content/themes/suscong/index.php
+++ wp-content/themes/suscong/index.php
@@ -1,51 +1,7 @@
 <?php get_header(); ?>
 
 <section id="main">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4 project">
-				<h4>Company Profile</h4>
-				<p>Dongguan Suscong Healthcare Co., Ltd is an internationally inspired company that creates and markets Lifestyle Products, including Foot Care Accessories, Body Care and Medical Products, along with alternative accessories used across all areas of everyday living. 
-					<a href=""> Leer m&aacute;s.</a>
-				</p>
-			</div>
-			<div class="col-md-4 project">
-				<h4>Team</h4>
-				<p>Suscong works very diligently to provide a complete professional product and service to all our clients. In order for us to fulfill this goal we realize that teamwork and communication are the most important components. Our staff is like a family who all work together for the common good. 
-					<a href=""> Leer m&aacute;s.</a>
-				</p>
-			</div>
-			<div class="col-md-4 project">
-				<h4 style="margin-left: 20px;">Advantage</h4>
-				<p>At suscong we recognize how difficult it is for foreign companies to buy and receive a good standard of customer service in China. The difficulties are many fold; involving everything from lack of contact and information to language, delivery times and quality issues. Buying products from many different manufacturing plants can be a nightmare waiting to happen. 
-					<a href=""> Leer m&aacute;s.</a>
-				</p>
-			</div>
-		</div>
-	</div>
-
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4 project">
-				<h4>Customer Satisfaction</h4>
-				<p>The satisfaction of our customers is our primary objective.<br> Suscong strives to attain these objectives by supplying the best quality and service. Fulfilling expectations via continuous company development with R&D and customer service instrumental in our progressive future path.
-				</p>
-			</div>
-
-			<div class="col-md-4 project">
-				<h4>What do we do?</h4>
-				<p>* Sourcing.<br>* Manufacturing / Production.<br> * Packaging Solutions.<br> * Project Solutions.<br> * Research & Development.<br> * Product Enhancement.<br>* Brand Management.<br>* Quality Control.<br>* International Testing Requirement.<br>* Delivery Options.
-				</p>
-			</div>
-			<div class="col-md-4 project">
-				<h4 style="margin-left: 20px;">Suscong is on Alibaba</h4>
-				<p>
-					<a href="www.alibaba.com">www.alibaba.com</a> 
-					a makes it easy for millions of buyers and suppliers around the world to do business online through a global trade platform for importers and exporters. Please go to our products section to direct access.
-				</p>
-			</div>
-		</div>
-	</div>
+	
 
 	<!--about start-->    
 
